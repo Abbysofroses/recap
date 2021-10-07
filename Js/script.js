4ac1747badddd5698b8ea76fd47b89c7956f7bec
Pasha <penaltyoffice@gmail.com>
2021-10-07 22:34:31 +0400

    practice-1

--- Js/script.js
+++ Js/script.js
@@ -1,8 +1,21 @@
-const category = 'toys';
+const numberOffilms = +prompt('Сколько фильмов вы уже посмотрели', '');
 
-console.log(`https://someurl.com/${category}/5`);
+const personalMovieDB = {
 
-const user = 'Ivan';
+count: numberOffilms,
+movies: {},
+actors: {},
+genres: [],
+privat: false
 
-alert(`Hi, ${user}`);
+};
 
+const a = prompt('Last movie', ''),
+      b = prompt('rate', ''),
+      c = prompt('Last movie', ''),
+      d = prompt('rate', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
